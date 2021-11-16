bcc3e4dc14e61c5380674d53ee561bbdd42916f0
Felipe de Oliveira <felipeoliveiraoliveira92@gmail.com>
2021-11-16 15:37:07 -0300

    Melhorando o seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,37 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Coin.create!(
-  description: "Bitcoin",
-  acronym: "BTC",
-  url_image: "https://cdn.pixabay.com/photo/2019/06/23/19/15/bitcoin-4294492_1280.png"
-)
+spinner = TTY::Spinner.new("[:spinner] Cadastrando Moedas...") 
+spinner.auto_spin
 
-Coin.create!(
-  description: "Ethereum",
-  acronym: "ETH",
-  url_image: "https://www.batistacoin.net/wp-content/uploads/2017/06/ethereum.png"
-)
+coins = [
+  {
+    description: "Bitcoin",
+    acronym: "BTC",
+    url_image: "https://cdn.pixabay.com/photo/2019/06/23/19/15/bitcoin-4294492_1280.png"
+  },
+  
+  {
+    description: "Ethereum",
+    acronym: "ETH",
+    url_image: "https://www.batistacoin.net/wp-content/uploads/2017/06/ethereum.png"
+  },
 
-Coin.create!(
-  description: "Dash",
-  acronym: "DASH",
-  url_image: "https://s2.coinmarketcap.com/static/img/coins/200x200/131.png"
-)
+  {
+    description: "Dash",
+    acronym: "DASH",
+    url_image: "https://s2.coinmarketcap.com/static/img/coins/200x200/131.png"
+  },
 
-Coin.create!(
-  description: "Monero",
-  acronym: "XMR",
-  url_image: "https://s2.coinmarketcap.com/static/img/coins/200x200/328.png"
-)
+  {
+    description: "Monero",
+    acronym: "XMR",
+    url_image: "https://s2.coinmarketcap.com/static/img/coins/200x200/328.png"
+  }
+]
+
+coins.each do |coin|
+  Coin.find_or_create_by!(coin)
+end
+
+spinner.success("(Concluído!)")
