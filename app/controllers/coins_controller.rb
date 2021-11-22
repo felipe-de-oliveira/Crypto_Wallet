f51d703bbbe682a6e3b9d264d7d020e35c01c97f
Felipe de Oliveira <felipeoliveiraoliveira92@gmail.com>
2021-11-22 16:13:34 -0300

    aplicando o fluxo MVC no helper select

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,5 +1,6 @@
 class CoinsController < ApplicationController
-  before_action :set_coin, only: %i[ show edit update destroy ]
+  before_action :set_coin, only: %i[show edit update destroy]
+  before_action :set_mining_type_options, only: [:new, :create, :edit, :update]
 
   # GET /coins or /coins.json
   def index
@@ -57,6 +58,11 @@ class CoinsController < ApplicationController
   end
 
   private
+
+    def set_mining_type_options
+      @mining_type_opcions = MiningType.all.pluck(:description, :id)
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_coin
       @coin = Coin.find(params[:id])
