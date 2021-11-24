ae93262fb907ee5b4dbadfafaab84757e382c210
Felipe de Oliveira <felipeoliveiraoliveira92@gmail.com>
2021-11-24 13:40:09 -0300

    adicionando a gem I18n ao projeto

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def locale(locale)
+    I18n.locale == :en ? "Estados Unidos" : "Portuguẽs do Brasil"
+  end
+
   def data_brasileira(data_us)
     data_us.strftime("%d/%m/%Y")  
   end
